02900af9dd431f81622e2a9ad2c609aba2990520
her7man <herman.andriam@gmail.com>
2023-08-09 11:36:41 +0300

    Day 03

--- DataCollect.py
+++ DataCollect.py
@@ -1,39 +1,79 @@
 import streamlit as st
+import base64
+import re
+
 
 def main():
-    st.title("Welcome to Our Stroke Prediction App")
-    st.write("This is a simple example of a Streamlit app.")
-    st.write("Please feel free to complete the form to predict your situation.")
 
-    # Gender
-    gender = st.sidebar.selectbox("What is your gender?", ["Male","Female", "Other"])
+    # Load the SVG file
+    with open("data/images/undraw_medicine_b1ol.svg", 'r') as f:
+        svg = f.read()
+
+    # Remove width and height attributes from the SVG
+    svg = re.sub(r'(<svg[^>]*?) (width="[^"]*"|height="[^"]*")', r'\1', svg, flags=re.DOTALL)
+
+    # Wrap the SVG in a container and style it to be responsive
+    svg_container = f"""
+    <div style="width:50%; height:auto; margin-left:auto; margin-right:auto;">
+        {svg}
+    </div>
+    """
     
-    # Mariage Situation 
-    ever_married = st.sidebar.selectbox("Have you ever married", ["Yes", "No"])
+    # Display the SVG in Streamlit
+    st.markdown(svg_container, unsafe_allow_html=True)
 
-    # Age of the client
-    age = st.sidebar.slider("Enter your age", 1, 100, 40)
+    # st.image("data/images/undraw_medicine_b1ol.png", caption='Your Image Caption', use_column_width=True)
+    st.title("Welcome to Our Stroke Prediction App")
+    st.write("Understanding your risk factors for stroke can help you take proactive steps towards health.")
+
+    # Sidebar: About Patient
+    with st.sidebar:
+        st.header("About Patient")
+
+        # Gender
+        gender = st.selectbox("What is your gender?", ["Male","Female", "Other"])
+
+        # Marriage Situation 
+        ever_married = st.selectbox("Have you ever been married?", ["Yes", "No"])
+
+
+        # Age of the client
+        age = st.slider("Enter your age", 1, 100, 40)
+
+        # Height & Weight
+        height = st.slider("Your height (cm)", 1, 280, 170)
+        weight = st.slider("Your weight (kg)", 1, 280, 75)
+
+        # Calculate BMI
+        bmi_avg = float(weight / (height/100)**2)  # converting height from cm to meters
+        st.text(f"Your BMI is: {bmi_avg:.2f}")
+
+    # Main screen: Health Information
+    st.header("Health Information")
 
     # Hypertension
-    hypertension = st.selectbox("Do you have hypertension", ["Yes", "No"])
+    hypertension = st.selectbox("Do you have hypertension?", ["Yes", "No"])
 
     # Heart Diseases
-    heartdisease = st.selectbox("Do you have Heart Disease", ["Yes", "No"])
+    heartdisease = st.selectbox("Do you have any heart diseases?", ["Yes", "No"])
 
     # Work Type
-    work_type = st.selectbox("What is your work type", ["Private", "Self Employed", "Government", "Children"])
+    work_type = st.selectbox("What's your occupation?", ["Private", "Self Employed", "Government", "Children"])
 
     # Residence Type
-    Residence_type = st.selectbox("Which type of residence do you live", ["Urban","Rural"])
+    Residence_type = st.selectbox("Where do you reside?", ["Urban","Rural"])
 
     # Glucose Average
-    avg_glucose_level = st.slider("Your Glucose Average in mg/dl", 1, 600, 256)
+    avg_glucose_level = st.slider("Your average glucose level (mg/dl)", 1, 600, 256)
 
     # Smoking Status
-    smoking_status = st.selectbox("Are you a smoker", ["Yes", "No"])
+    smoking_status = st.selectbox("Do you smoke?", ["Yes", "No"])
 
-    height = st.sidebar.slider("Your height in cm", 1, 280, 170)
-    weight = st.sidebar.slider("Your weight in kg", 1, 280, 75)
-    bmi_avg = float(weight / (height ** 2))
-    return
+    st.write("Once you've filled out the above details, click below for your assessment!")
+    if st.button("Get My Assessment"):
+        # Placeholder result
+        st.write("Your stroke risk assessment will appear here!")
+
+
+
 main()
